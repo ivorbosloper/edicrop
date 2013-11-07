653862f307c28f53eca72fdcfaa0bf7787e8d0a5
Ivor Bosloper <ivorbosloper@gmail.com>
2013-11-07 09:36:18 +0100

    Update simple project to new/current xsds and WSDL

--- simple/src/nl/agroconnect/EdiPortTypeImpl.java
+++ simple/src/nl/agroconnect/EdiPortTypeImpl.java
@@ -1,16 +1,12 @@
 package nl.agroconnect;
 
-import static nl.agroconnect.Util.code;
 import static nl.agroconnect.Util.id;
-import static nl.agroconnect.Util.nl;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
-import nl.agroconnect.wsEdiCrop.v4_0.EdiCropVersionType;
 import nl.agroconnect.wsEdiCrop.v4_0.EdiPortType;
 import nl.agroconnect.wsEdiCrop.v4_0.GetCropRecordingListRequest;
 import nl.agroconnect.wsEdiCrop.v4_0.GetCropRecordingListResponse;
@@ -21,17 +17,11 @@ import nl.agroconnect.wsEdiCrop.v4_0.GetCroppingSchemeListResponse;
 import nl.agroconnect.wsEdiCrop.v4_0.GetCroppingSchemeRequest;
 import nl.agroconnect.wsEdiCrop.v4_0.GetCroppingSchemeResponse;
 import nl.agroconnect.wsEdiCrop.v4_0.IDType;
-import nl.agroconnect.wsEdiCrop.v4_0.MessageTypeType;
 import nl.agroconnect.wsEdiCrop.v4_0.PutCropRecordingRequest;
 import nl.agroconnect.wsEdiCrop.v4_0.PutCropRecordingResponse;
 import nl.agroconnect.wsEdiCrop.v4_0.PutCroppingAdviceRequest;
 import nl.agroconnect.wsEdiCrop.v4_0.PutCroppingAdviceResponse;
-import nl.agroconnect.wsEdiCrop.v4_0.YesNoType;
 import nl.agroconnect.wsEdiCrop.v4_0.crps.CroppingSchemeMessageType;
-import nl.agroconnect.wsEdiCrop.v4_0.crps.DocumentType;
-import nl.agroconnect.wsEdiCrop.v4_0.crps.FarmClassificationType;
-import nl.agroconnect.wsEdiCrop.v4_0.crps.FarmType;
-import nl.agroconnect.wsEdiCrop.v4_0.crps.SpecifiedPartyType;
 
 @javax.jws.WebService(
                       serviceName = "EdiService",
@@ -50,33 +40,14 @@ public class EdiPortTypeImpl implements EdiPortType {
     }
 
     public GetCroppingSchemeResponse getCroppingScheme(GetCroppingSchemeRequest payload) { 
-    	SpecifiedPartyType farmContact = Data.farmContactWithId(payload.getFarmID());
-    	DocumentType doc = new DocumentType()
-				.withID(id(payload.getFarmID().getValue() + "_" + payload.getMessageId()))
-				.withType(MessageTypeType.CRPSCH)
-				.withEdiCropVersion(EdiCropVersionType.CRP_4_0)
-				.withMessageTypeVersion("4.0")
-				.withIssueDate(new Date())
-				.withCropYear(new BigDecimal(2013))
-				.withReportCount(new BigDecimal(0.0))
-				.withIssuer(farmContact) 		// de teler die het voorgenomen bouwplan afgeeft (FarmID / KVK)
-				.withSender(Data.bms) 			// het systeem van waaruit het bouwplan wordt opgeleverd, bijvoorbeeld Agrovision of Dacom
-				.withReceiver(Data.receiver) 	// voor wie het bericht bedoeld is, in dit geval de aanduiding van het adviessysteem
-				.withDescription(nl("Voorbeeld CroppingScheme bericht voor testboerderij"));
-				
-    	FarmType farm = new FarmType()
-    		.withFarmContact(farmContact)
-    		.withThirdPartyFarmID(new IDType().withSchemeAgencyName("DACOM").withValue("1"))
-    		.withFarmClassification(new FarmClassificationType(code("1"), YesNoType.N));
-    	Data.addFieldsTo(farm.getField());
-
-    	CroppingSchemeMessageType msg = new CroppingSchemeMessageType(doc, farm);
+    	CroppingSchemeMessageType msg = CrpsData.message(payload);
     	return new GetCroppingSchemeResponse(0, payload.getMessageId(), new BigDecimal(2013), msg);
     }
 	
     public GetCropRecordingListResponse getCropRecordingList(GetCropRecordingListRequest payload) { 
-        GetCropRecordingListResponse _return = null;
-        return _return;
+		List<IDType> farms = new ArrayList<IDType>();
+		farms.add(id("1"));
+		return new GetCropRecordingListResponse(0, payload.getMessageId(), new BigDecimal(2013), farms);
     }
 
     public GetCropRecordingResponse getCropRecording(GetCropRecordingRequest payload) { 
--- simple/src/nl/agroconnect/Util.java
+++ simple/src/nl/agroconnect/Util.java
@@ -1,5 +1,6 @@
 package nl.agroconnect;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -7,11 +8,12 @@ import java.util.regex.Pattern;
 
 import nl.agroconnect.wsEdiCrop.v4_0.CodeType;
 import nl.agroconnect.wsEdiCrop.v4_0.IDType;
+import nl.agroconnect.wsEdiCrop.v4_0.MeasureType;
+import nl.agroconnect.wsEdiCrop.v4_0.PolygonType;
 import nl.agroconnect.wsEdiCrop.v4_0.TextType;
-import nl.agroconnect.wsEdiCrop.v4_0.crps.AbstractRingPropertyType;
-import nl.agroconnect.wsEdiCrop.v4_0.crps.DirectPositionListType;
-import nl.agroconnect.wsEdiCrop.v4_0.crps.LinearRingType;
-import nl.agroconnect.wsEdiCrop.v4_0.crps.PolygonType;
+import nl.agroconnect.wsEdiCrop.v4_0.AbstractRingPropertyType;
+import nl.agroconnect.wsEdiCrop.v4_0.DirectPositionListType;
+import nl.agroconnect.wsEdiCrop.v4_0.LinearRingType;
 
 public class Util {
     protected static TextType nl(String s) {
@@ -26,12 +28,20 @@ public class Util {
     	return new CodeType().withValue(s);
     }
 
+    protected static BigDecimal dec(Double val) {
+    	return BigDecimal.valueOf(val);
+    }
+
+    protected static MeasureType mt(Double val) {
+    	return new MeasureType().withValue(BigDecimal.valueOf(val));
+    }
+    
     /**
      * Convert WKT ring to JAXB serializable formqt
      * @param wktRing WKT description of a ring,  e.g. (30 10, 10 20, 20 40, 40 40, 30 10)
      * @return ring converted to AbstractRingPropertyType
      */
-    protected static AbstractRingPropertyType abstractRing(String wktRing) {
+    static AbstractRingPropertyType abstractRing(String wktRing) {
     	String posList = wktRing.substring(1,wktRing.length()-1).replace(",", "");
 		AbstractRingPropertyType ring = new AbstractRingPropertyType(new LinearRingType(new DirectPositionListType().withValue(posList).withSrsName("EPSG:4326")));
 		return ring;
@@ -42,7 +52,7 @@ public class Util {
      * @param wktPolygon WKT description of the Polygon, e.g. POLYGON ((30 10, 10 20, 20 40, 40 40, 30 10))
      * @return polygon 
      */
-	protected static PolygonType polygon(String wktPolygon) {
+	static PolygonType polygon(String wktPolygon) {
         Pattern pattern = Pattern.compile("POLYGON\\s*\\((?:(\\([\\d\\s\\.,]+\\)),?)+\\)\\s*$");
         Matcher m = pattern.matcher(wktPolygon);
         PolygonType p = null;
@@ -55,5 +65,5 @@ public class Util {
         	p = new PolygonType(abstractRing(m.group(1)), null);
         }
 		return p;
-	}
+	}	
 }
