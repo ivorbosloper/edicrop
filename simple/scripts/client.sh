6425a8f2dc4bd3bdd8ba2fb3193e0d12b76afd6f
Ivor Bosloper <ivorbosloper@gmail.com>
2013-11-07 16:37:02 +0100

    Added example data and example messages

--- simple/scripts/client.sh
+++ simple/scripts/client.sh
@@ -5,5 +5,15 @@ if [ $# -gt 0 ]; then
     endpoint=$1
 fi
 
+echo sending resources/example/croppingSchemeListRequest.xml...
+cat resources/example/croppingSchemeListRequest.xml
 cat resources/example/croppingSchemeListRequest.xml | curl -D - -o resources/example/croppingSchemeListResponse.xml -X POST -H 'Content-type: text/xml' -H 'SOAPAction: "getCroppingSchemeList"' -d "@-" $endpoint
+echo received:
+cat resources/example/croppingSchemeListResponse.xml
+
+echo
+echo sending resources/example/croppingSchemeRequest.xml...
+cat resources/example/croppingSchemeRequest.xml
 cat resources/example/croppingSchemeRequest.xml | curl -D - -o resources/example/croppingSchemeResponse.xml -X POST -H 'Content-type: text/xml' -H 'SOAPAction: "getCroppingScheme"' -d "@-" $endpoint
+echo received:
+cat resources/example/croppingSchemeResponse.xml
